06252ab74fee3e1e6a9622b4ffc90d0e9a832560
Opilane TTHK <opilane@tthk.ee>
2021-10-26 10:12:31 +0300

    jou

--- epicfight/Program.cs
+++ epicfight/Program.cs
@@ -8,16 +8,64 @@ namespace epicfight
         {
             string hero = pickhero();
             string villain = pickvillain();
+            int herohp = generatehp(hero);
+            int villainhp = generatehp(villain);
+
             Console.WriteLine($"{hero} will fight {villain}.");
 
             string heroweapon = pickweapon();
             string villainweapon = pickweapon();
-            Console.WriteLine($"{hero} will fight {villain}.");
+            Console.WriteLine($"{hero} picked {heroweapon}. {villain} picked {villainweapon}.");
+
+            while(herohp > 0 && villainhp > 0)
+            {
+                herohp = herohp - Hit(villain, hero, villainweapon);
+                villainhp = villainhp - Hit(hero, villain, heroweapon);
+
+            }
+            if (herohp > 0)
+            {
+                Console.WriteLine("darkside wins");
+            }
+            else
+            {
+                Console.WriteLine($"{hero} saves the day!");
+            }
+
+            
 
         }
+
+        private static int Hit(string charecterone, string charectertwo, string someweapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, someweapon.Length / 2);
+
+            Console.WriteLine($"{charecterone} hit {strike}");
+            if(strike == someweapon.Length / 2)
+            {
+                Console.WriteLine($"awesome! {charectertwo} made a critical hit!");
+            }
+            else if (strike == 0)
+            {
+                Console.WriteLine($"{charectertwo} dodged the attack!");
+            }
+            return strike;
+
+        }
+
+
+
+
+        private static int generatehp(string somename)
+        {
+            Random rnd = new Random();
+            return rnd.Next(somename.Length, somename.Length + 10);
+        }
+
         private static string pickweapon()
         {
-            string[] weapon = { "hamer", "hands", "bat", "bomb", "banana" };
+            string[] weapon = { "dogge", "beniz", "ak-47", "keyboard", "banana" };
             Random rnd = new Random();
             int randomindex = rnd.Next(0, weapon.Length);
 
